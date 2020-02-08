e4d509e3c068e2cc5f42bb770434f124511d7357
Vimal Raj N C <60806437+vimal-maker@users.noreply.github.com>
2020-02-08 15:49:21 +0530

    Add files via upload

---
+++ C#_ENTITYFRAMEWORK/entityt/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+
+namespace codefirstdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            productDBContext context = new productDBContext();
+            var product = context.Products;
+            var product1 = product.Add(new product() { productId = 1, Name = "badam drink", price = 20.99, quantity = "150ml", categoryId = 1 });
+            var product2 = product.Add(new product() { productId = 2, Name = "coffee drink", price = 49.99, quantity = "250ml", categoryId = 2 });
+            var product3 = product.Add(new product() { productId = 3, Name = "coke drink", price = 50.99, quantity = "200ml", categoryId = 3 });
+
+
+         
+            context.SaveChanges();
+
+
+
+        }
+    }
+    public class productDBContext : DbContext
+    {
+        public productDBContext() : base("name=productDBContext")
+        {
+
+        }
+        public DbSet<product> Products { get; set; }
+        public DbSet<category> categories { get; set; }
+    }
+    public class product
+    {
+        public int productId { get; set; }
+        public string Name { get; set; }
+        public double price { get; set; }
+        public string quantity { get; set; }
+        public int categoryId { get; set; }
+    }
+    public class category
+    {
+        public int categoryID { get; set; }
+        public string name { get; set; }
+        public string department{ get; set; }
+    }
+}
+
+//    public class student
+//    {
+//        public int studentID { get; set; }
+//        public string name { get; set; }
+//    }
+//    public class teacher
+//    {
+//        public int teacherID { get; set; }
+//        public string name { get; set; }
+       
+
+//    }
+//}
---
+++ C#_ENTITYFRAMEWORK/frameworkentity/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frameworkentity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            studentEFDBEntities context = new studentEFDBEntities();
+            //var Student = new student()
+            //{
+            //    StdID = 1,
+            //    name = "vimal",
+            //    Rollno = 15117,
+            //    DeptID = 2
+
+            //};
+            //context.students.Add(Student);
+            //context.SaveChanges();
+
+            //var Student2 = new student()
+            //{
+            //    StdID = 2,
+            //    name = "mohan",
+            //    Rollno = 15116,
+            //    DeptID = 3
+
+            //};
+            //context.students.Add(Student2);
+
+
+            //var Student3 = new student()
+            //{
+            //    StdID = 3,
+            //    name = "anup",
+            //    Rollno = 15118,
+            //    DeptID = 1
+
+            //};
+            //context.students.Add(Student3);
+            //context.SaveChanges();
+
+
+            LinqToEntity();
+            ModifyStudent();
+        }
+        public static void LinqToEntity()
+        {
+            studentEFDBEntities context = new studentEFDBEntities();
+            var student = context.student;
+            var stdlist = from std in student
+                          orderby std.Department
+                          select std;
+            foreach(var std in student)
+            {
+                Console.WriteLine("StdID:{0},name:{1},Rollno:{2}", std.StdID, std.name, std.Rollno);
+            }
+        }
+        public static void ModifyStudent()
+        {
+            studentEFDBEntities context = new studentEFDBEntities();
+            var students = context.student;
+            var stdlist = from std in students
+                          orderby std.Rollno
+                          select std;
+            foreach(var std in students)
+            {
+                if(std.StdID == 2)
+                    std.name = "soona pana";
+
+            }
+              context.student.Add(students);
+            context.SaveChanges();
+        }
+    }
+}
